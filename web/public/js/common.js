f0af9250f212058d0865fe208f6499f4edd0664d
amderbar <amderbar@gmail.com>
2017-07-22 10:46:27 +0900

    ライブラリの改善
    * HTML Utilのselectbox出力関数を便利にした
    * なんちゃってViewテンプレートエンジンを実装
    * 共通jsにオブジェクトに対して適用できるmap関数を作成
    * modal用jsを共通から切り出し
    * mainのiframeをブロック要素化
    * textareaをブロック要素化

--- web/public/js/common.js
+++ web/public/js/common.js
@@ -1,31 +1,9 @@
-var modal;
-var overlay = document.getElementById('modal-overlay');
-
-/**
- * モーダルを開く
- */
-function openModal(button){
-    button.blur();
-    switch (button.id) {
-    case 'proj-Btn':
-        modal = document.getElementById('new-proj');
-        break;
-    case 'tmpl-Btn':
-        modal = document.getElementById('new-tmpl');
-        break;
+function hashMap(hash, callback) {
+    var new_hash = {};
+    for (let key in hash) {
+        if (hash.hasOwnProperty(key)) {
+            new_hash[key] = callback(hash[key], key);
+        }
     }
-    modal.style.visibility = 'visible';
-    overlay.style.visibility = 'visible';
+    return new_hash;
 }
-
-/**
- * モーダルを閉じる
- */
-function hideModal() {
-    // モーダルベースレイヤを非表示にする
-    modal.style.visibility = 'hidden';
-    overlay.style.visibility = 'hidden';
-    modal = null;
-}
-
-overlay.addEventListener('click', hideModal,false);
