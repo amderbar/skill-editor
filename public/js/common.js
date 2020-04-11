03d9009b1e68c17531e2b85350dc1abf7b376a7d
amderbar <amderbar@gmail.com>
2020-04-11 17:18:03 +0900

    JS関連ファイルの削除等

--- public/js/common.js
+++
@@ -1,9 +0,0 @@
-function hashMap(hash, callback) {
-    var new_hash = {};
-    for (let key in hash) {
-        if (hash.hasOwnProperty(key)) {
-            new_hash[key] = callback(hash[key], key);
-        }
-    }
-    return new_hash;
-}
--- resources/assets/js/components/data-editor/FormComponentMixin.js
+++
@@ -1,6 +0,0 @@
-export default {
-    props: {
-        name: String,
-        value: null
-    }
-}
